3ece5a6c1fe5e0cde0a9e46754d78974f10e40bc
qodriaziz <61813572+qodriaziz@users.noreply.github.com>
2023-06-08 18:15:34 +0700

    menambah method untuk data get current location

--- app/src/main/java/com/example/tugasmaps/MainActivity.java
+++ app/src/main/java/com/example/tugasmaps/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -13,19 +14,34 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+//import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.Manifest;
 import android.provider.Settings;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 boolean isPersmissionGranter;
+
+SupportMapFragment fragmentMap;
+FusedLocationProviderClient locationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
+        getCurrentLocation();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        fragmentMap = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        locationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
+
     }
+
 
     private void checkPermission() {
         Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
@@ -50,4 +66,9 @@ boolean isPersmissionGranter;
             }
         }).check();
     }
+
+    private void getCurrentLocation() {
+    }
+
+
 }
