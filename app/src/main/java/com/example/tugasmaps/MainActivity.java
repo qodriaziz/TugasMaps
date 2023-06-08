fa3762cfcfbfaf47c98919bcb37a9c5086b22685
qodriaziz <61813572+qodriaziz@users.noreply.github.com>
2023-06-08 17:02:26 +0700

    buar metod permisioon

--- app/src/main/java/com/example/tugasmaps/MainActivity.java
+++ app/src/main/java/com/example/tugasmaps/MainActivity.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+import android.Manifest;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+boolean isPersmissionGranter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPersmissionGranter = true;
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
     }
 }
